ef071d8c4c3bd53d8c8edc83533882227933edcc
RZN <68141255+rajsadian@users.noreply.github.com>
2024-04-24 16:47:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// Tugas Kecil
-// 1. Prompt akan mengetahui saldo akhir yang di input user
-let balanceIn = prompt("please input your balance in");
-alert(`your balance in is RP.${balanceIn}`);
-let balanceOut = prompt("please input your balance out");
-alert(`your balance out is RP.${balanceOut}`);
-const balanceLeft = balanceIn - balanceOut;
-alert(`your balance left is ${balanceLeft}`);
-// 2. menentukan hari / tanggal yang ada saat ini di real life
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const timeStamp = new Date().getDay();
-const daysName = days[timeStamp];
-alert(`this day was ${daysName}`);
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
